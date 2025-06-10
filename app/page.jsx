fc9a26e6678682df29d70124bd794c280d20c40f
Tarık İslamoğlu <tarikislamoglu8@gmail.com>
2025-06-10 21:41:38 +0300

    firstupdate

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,7 @@
 "use client";
+
+import LoginPage from "./login/page";
+
 export default function Home() {
-  return (
-    <div className="flex flex-col justify-center items-center">
-      <h1>Ana Sayfa</h1>
-      <a href="/register">Kayıt Ol</a>
-      <a href="/login">Giriş Yap</a>
-    </div>
-  );
+  return <LoginPage />;
 }
